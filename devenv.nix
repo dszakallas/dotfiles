fe7629487aae8a1bff22c4283f5642ebb381fb32
Dávid Szakállas <david.szakallas@gmail.com>
2025-02-15 17:37:08 +0100

    use davids-devenvs

--- devenv.nix
+++ devenv.nix
@@ -1,14 +1,1 @@
-{ ... }:
-{
-  languages.nix.enable = true;
-  pre-commit.hooks = {
-    nixfmt-rfc-style.enable = true;
-    markdownlint = {
-      enable = true;
-      settings.configuration = {
-        MD013.line_length = 120;
-      };
-    };
-    shellcheck.enable = true;
-  };
-}
+{ ... }: { }
