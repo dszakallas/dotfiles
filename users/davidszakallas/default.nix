d915f28418eab44a0b1b2e9e1f94cfb48bd7117a
Dávid Szakállas <5807322+dszakallas@users.noreply.github.com>
2026-07-05 13:57:13 +0200

    users: add gogcli from nixpkgs to home packages

--- users/davidszakallas/default.nix
+++ users/davidszakallas/default.nix
@@ -58,6 +58,7 @@
           yt-dlp
           google-cloud-sdk
           playwright-mcp
+          gogcli
         ]);
       };
 
--- users/dszakallas/default.nix
+++ users/dszakallas/default.nix
@@ -43,6 +43,7 @@
         ++ (with pkgs; [
           fluxcd-operator
           temporal-cli
+          gogcli
         ]);
       };
 
